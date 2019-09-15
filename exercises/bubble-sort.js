1049a7541fe3d07244527167422a3361998237ca
Alexey Duryagin <aiduryagih@gmail.com>
2019-09-15 12:15:25 +0300

    bubble sort template fix

--- exercises/bubble-sort.js
+++ exercises/bubble-sort.js
@@ -1,3 +1,9 @@
+/*
+  Требуется написать bubble sort
+
+  [3, 2, 5, 6, 7, -1] => [-1, 2, 3, 5, 6, 7]
+*/
+
 function bubble (arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let q = i + 1; q < arr.length; q++) {
