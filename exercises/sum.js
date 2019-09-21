546d01bf273fd5fde90736a656e1b9cd4469f97c
Alexey Duryagin <aiduryagih@gmail.com>
2019-09-21 11:53:57 +0300

    sum

---
+++ exercises/sum.js
@@ -0,0 +1,26 @@
+/**
+ * Функция для суммы аргументов
+ * f(1)(2)(3) => 6
+ */
+
+function sum (a) {
+  let result = a
+
+  function sum2 (b) {
+    result += b
+
+    return sum2
+  }
+
+  sum2.valueOf = () => {
+    return result
+  }
+
+  sum2.toString = () => {
+    return result
+  }
+
+  return sum2
+}
+
+console.log(sum(1)(2)(3))
